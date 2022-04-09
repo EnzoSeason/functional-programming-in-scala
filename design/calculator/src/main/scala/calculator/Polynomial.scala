66840e7c97c57881868e069b8b04c7279523c986
Jijie LIU <liujijieseason@gmail.com>
2022-04-09 10:50:17 +0200

    feat: complete Polynomial

--- design/calculator/src/main/scala/calculator/Polynomial.scala
+++ design/calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,8 +3,12 @@ package calculator
 object Polynomial extends PolynomialInterface:
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] =
-    ???
+    Signal(b() * b() - 4 * a() * c())
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] =
-    ???
+    Signal{
+      val root1 = (-b() + math.sqrt(delta())) / (2 * a())
+      val root2 = (-b() - math.sqrt(delta())) / (2 * a())
+      Set(root1, root2)
+    }
